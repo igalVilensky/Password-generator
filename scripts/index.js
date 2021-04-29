9265b73412f67ebc937b0b248ba10b61f97d67d0
igalv <vilenskyigal@gmail.com>
2021-04-29 11:04:23 +0200

    Updates

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,26 @@
+const userNum = document.querySelector("#userInput");
+console.log(userNum.value);
+/* const resultPass = document.addEventListener("result-container"); */
+
 function makeid(num) {
+  num = userNum.value;
   let result = "";
   let characters =
     "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
   let charactersLength = characters.length;
-  if (12 < num || num < 8)
-    return "THe password should be between 8 and 12 character";
+  console.log(num);
+
+  if (12 < num || num < 8) {
+    document.querySelector(".result-container").style.marginTop = "1.3rem";
+    return (document.querySelector(".result-container").innerHTML =
+      "The password should be between 8 and 12 character");
+  }
 
   for (let i = 0; i < num; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
+    let newEll = document.createElement("h1");
+    newEll.innerHTML = result;
+    newEll.classList.add("result-container");
   }
-  return result;
+  return (document.querySelector(".result-container").innerHTML = result);
 }
-
-console.log("-----------------------------");
-console.log(makeid(1));
-console.log(makeid(200));
-console.log(makeid(12));
